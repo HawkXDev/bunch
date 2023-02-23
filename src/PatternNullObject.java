07068ef53296a1d2695c8a5e7b2571d1bd73ff48
sokolserg <sergsokolkin@gmail.com>
2023-02-23 09:48:54 +0300

    PatternNullObject

---
+++ src/PatternNullObject.java
@@ -0,0 +1,30 @@
+/**
+ * Pattern Null Object
+ */
+public class PatternNullObject {
+    public static void main(String[] args) {
+        AbstractEntity realEntity = new RealEntity();
+        realEntity.doSomething();  // RealEntity::doSomething
+
+        AbstractEntity unknownEntity = new NullEntity();
+        unknownEntity.doSomething();  // no output
+    }
+}
+
+abstract class AbstractEntity {
+    public abstract void doSomething();
+}
+
+class RealEntity extends AbstractEntity {
+    @Override
+    public void doSomething() {
+        System.out.println("RealEntity::doSomething");
+    }
+}
+
+class NullEntity extends AbstractEntity {
+    @Override
+    public void doSomething() {
+
+    }
+}
